3759b331b7cc007c64a48d8e7eff978db300acd9
Jonathan Hardison <jmh@jonathanhardison.com>
2015-08-29 13:01:42 -0500

    Assembly info change.

--- Source/GlassRoom/GlassRoom/Properties/AssemblyInfo.cs
+++ Source/GlassRoom/GlassRoom/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GlassRoom")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015 Jonathan Hardison")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
